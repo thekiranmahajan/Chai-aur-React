6892b727c7385224130b63e0ef5cbd671d81403f
Kiran Mahajan <91893931+thekiranmahajan@users.noreply.github.com>
2023-09-30 11:24:18 +0530

    added button animations n custom font

--- bgChanger/src/App.jsx
+++ bgChanger/src/App.jsx
@@ -9,90 +9,78 @@ function App() {
     >
       <div className="flex flex-wrap justify-center fixed bottom-12 inset-x-0 px-2">
         <div
-          className="flex justify-center flex-wrap gap-3 shadow-md bg-white px-3 py-2 rounded-3xl
+          className="flex justify-center flex-wrap gap-3 shadow-lg shadow-slate-600 bg-slate-400  px-5 py-3 rounded-3xl  font-poppins
       "
         >
           <button
             onClick={() => setColor("red")}
-            className="outline-none px-4 py-1 rounded-full text-white shadow-md shadow-slate-800 active:scale-90 "
-            style={{ backgroundColor: "red" }}
+            className=" px-4 py-2 rounded-full text-white shadow-md shadow-slate-800 active:scale-90 transform transition-transform duration-200 hover:scale-110 ease-in-out focus:ring-4 focus:outline-none focus:ring-red-400 bg-red-600"
           >
             Red
           </button>
           <button
             onClick={() => setColor("green")}
-            className="outline-none px-4 py-1 rounded-full text-white shadow-md shadow-slate-800 active:scale-90 "
-            style={{ backgroundColor: "green" }}
+            className="outline-none px-4 py-2 rounded-full text-white shadow-md shadow-slate-800 active:scale-90 transform transition-transform duration-200 hover:scale-110 ease-in-out focus:ring-4 focus:outline-none focus:ring-green-400 bg-green-600"
           >
             Green
           </button>
           <button
             onClick={() => setColor("blue")}
-            className="outline-none px-4 py-1 rounded-full text-white shadow-md shadow-slate-800 active:scale-90 "
-            style={{ backgroundColor: "blue" }}
+            className="outline-none px-4 py-2 rounded-full text-white shadow-md shadow-slate-800 active:scale-90 transform transition-transform duration-200 hover:scale-110 ease-in-out focus:ring-4 focus:outline-none focus:ring-blue-400 bg-blue-600"
           >
             Blue
           </button>
           <button
             onClick={() => setColor("olive")}
-            className="outline-none px-4 py-1 rounded-full text-white shadow-md shadow-slate-800 active:scale-90 "
-            style={{ backgroundColor: "olive" }}
+            className="outline-none px-4 py-2 rounded-full text-white shadow-md shadow-slate-800 active:scale-90 transform transition-transform duration-200 hover:scale-110 ease-in-out focus:ring-4 focus:outline-none focus:ring-lime-500 bg-[olive]"
           >
             Olive
           </button>
           <button
             onClick={() => setColor("gray")}
-            className="outline-none px-4 py-1 rounded-full text-white shadow-md shadow-slate-800 active:scale-90 "
-            style={{ backgroundColor: "gray" }}
+            className="outline-none px-4 py-2 rounded-full text-white shadow-md shadow-slate-800 active:scale-90 transform transition-transform duration-200 hover:scale-110 ease-in-out focus:ring-4 focus:outline-none focus:ring-gray-300 bg-gray-400"
           >
             Gray
           </button>
           <button
             onClick={() => setColor("orange")}
-            className="outline-none px-4 py-1 rounded-full text-white shadow-md shadow-slate-800 active:scale-90 "
-            style={{ backgroundColor: "orange" }}
+            className="outline-none px-4 py-2 rounded-full text-white shadow-md shadow-slate-800 active:scale-90 transform transition-transform duration-200 hover:scale-110 ease-in-out focus:ring-4 focus:outline-none focus:ring-orange-300 bg-orange-400"
           >
             Orange
           </button>
           <button
             onClick={() => setColor("yellow")}
-            className="outline-none px-4 py-1 rounded-full text-black shadow-md shadow-slate-800 active:scale-90 "
-            style={{ backgroundColor: "yellow" }}
+            className="outline-none px-4 py-2 rounded-full text-black shadow-md shadow-slate-800 active:scale-90 transform transition-transform duration-200 hover:scale-110 ease-in-out focus:ring-4 focus:outline-none focus:ring-yellow-200 bg-yellow-300"
           >
             Yellow
           </button>
           <button
             onClick={() => setColor("pink")}
-            className="outline-none px-4 py-1 rounded-full text-white shadow-md shadow-slate-800 active:scale-90 "
-            style={{ backgroundColor: "pink" }}
+            className="outline-none px-4 py-2 rounded-full text-black shadow-md shadow-slate-800 active:scale-90 transform transition-transform duration-200 hover:scale-110 ease-in-out focus:ring-4 focus:outline-none focus:ring-pink-50 bg-pink-200"
           >
             Pink
           </button>
           <button
             onClick={() => setColor("purple")}
-            className="outline-none px-4 py-1 rounded-full text-white shadow-md shadow-slate-800 active:scale-90 "
-            style={{ backgroundColor: "purple" }}
+            className="outline-none px-4 py-2 rounded-full text-white shadow-md shadow-slate-800 active:scale-90 transform transition-transform duration-200 hover:scale-110 ease-in-out focus:ring-4 focus:outline-none focus:ring-purple-300 bg-purple-500"
           >
             Purple
           </button>
           <button
             onClick={() => setColor("lavender")}
-            className="outline-none px-4 py-1 rounded-full text-black shadow-md shadow-slate-800 active:scale-90 "
-            style={{ backgroundColor: "lavender" }}
+            className="outline-none px-4 py-2 rounded-full text-black shadow-md shadow-slate-800 active:scale-90 transform transition-transform duration-200 hover:scale-110 ease-in-out focus:ring-4 focus:outline-none focus:ring-purple-200 bg-[lavender]"
           >
             Lavender
           </button>
           <button
             onClick={() => setColor("white")}
-            className="outline-none px-4 py-1 rounded-full text-black shadow-md shadow-slate-800 active:scale-90 "
-            style={{ backgroundColor: "white" }}
+            className="outline-none px-4 py-2 rounded-full text-black shadow-md shadow-slate-800 active:scale-90 transform transition-transform duration-200 hover:scale-110 ease-in-out focus:ring-4 focus:outline-none focus:ring-slate-200 bg-white"
           >
             White
           </button>
           <button
             onClick={() => setColor("black")}
-            className="outline-none px-4 py-1 rounded-full text-white shadow-md shadow-slate-800 active:scale-90 "
-            style={{ backgroundColor: "black" }}
+            className="outline-none px-4 py-2 rounded-full text-white shadow-md shadow-slate-800 active:scale-90 transform transition-transform duration-200 hover:scale-110 ease-in-out focus:ring-4 focus:outline-none focus:ring-slate-700 bg-black"
           >
             Black
           </button>
